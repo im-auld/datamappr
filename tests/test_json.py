5df41eace35756d581454b91f629261ee7def690
Ian Auld <imauld@gmail.com>
2014-06-24 12:36:55 -0700

    Refactored imports to account for directoy restructure.

--- tests/test_json.py
+++ tests/test_json.py
@@ -1,6 +1,17 @@
 import pytest
-from app.views import mock_ajax
+from datamapper.app import app
+from datamapper.app.views import mock_ajax
 
-def test_mock_ajax():
+
+@pytest.fixture(scope='session')
+def test_app():
+    datamapper.app.config['TESTING'] = True
+
+@pytest.yield_fixture(scope='session')
+def req_context():
+    with app.test_request_context('/mock_ajax'):
+        yield
+
+def test_mock_ajax(req_context):
     mock = mock_ajax()
     assert mock
