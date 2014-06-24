9cd783b019b48e1a472484d498f68902df55aba2
Ian Auld <imauld@gmail.com>
2014-06-24 14:26:38 -0700

    Added models to represent data in the the DB added necessary imports and declarations/initializations.

--- datamapper/app/__init__.py
+++ datamapper/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 app.config.from_object('config')
 
 import views, models
--- datamapper/app/models.py
+++ datamapper/app/models.py
@@ -1,13 +1,18 @@
-from flask.ext.sqlalchemy import sqlalchemy
+from datamapper.app import db
 
-
-db = SqlAlchemy()
 
 class State(db.Model):
-    pass
+    __tablename__ = 'State'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(20), nullable=False, unique=True)
+    short_name = db.Column(db.String(2), nullable=False, unique=True)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
 
 class Data(db.Model):
-    pass
-
-class DataType(db.Model):
-    pass
+    __tablename__ = 'Data'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    state_id = db.Column(db.Integer, db.ForeignKey('State.id'))
+    raw_data = db.Column(db.Float, nullable=False)
+    normalized_data = db.Column(db.Float)
+    date = db.Column(db.Date, nullable=False)
