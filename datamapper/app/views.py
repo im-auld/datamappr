393da814d0fbbb1624be9553c34e81bd8c6b3ee8
Ian Auld <imauld@gmail.com>
2014-06-25 13:47:15 -0700

    Expanded method to query DB

--- datamapper/app/views.py
+++ datamapper/app/views.py
@@ -2,14 +2,18 @@ from random import random, randint
 from flask import render_template, request, redirect, url_for
 from flask import jsonify
 from datamapper.app import app
-from datamapper.app.models import State
+from datamapper.app.models import State, Data
 from datamapper.app.forms import DataSetForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     form = DataSetForm()
-    form.data_set.choices = [(1,'Unemployment'), (2,'GDP'), (3,'Housing Starts')]
+    form.data_set.choices = [
+        ('Unemployment','Unemployment'),
+        ('GDP','GDP'),
+        ('Housing Starts','Housing Starts')
+        ]
     return render_template('index.html', form=form)
 
 @app.route('/mock-ajax')
@@ -29,7 +33,17 @@ def get_data_set():
     print(request.args)
     data_set = request.args.get('data_set', False)
     date_start = request.args.get('date_start', False)
-    date_end = request.args.get('date_end', True)
+    date_end = request.args.get('date_end', False)
+    if date_end:
+        result = Data.query\
+            .filter(Data.type == data_set)\
+            .filter(Data.date_gathered.between(date_start, date_end))\
+            .all()
+    else:
+        result = Data.query\
+            .filter(Data.type == data_set)\
+            .filter(Data.date_gathered >= date_start)\
+            .all()
     return jsonify(
         data_set=data_set,
         date_start=date_start,
