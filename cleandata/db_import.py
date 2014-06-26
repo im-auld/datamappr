1a6316171eefc8b5190e1f674191cafa417f8a1f
Ian Auld <imauld@gmail.com>
2014-06-26 15:29:40 -0700

    Updated import script for use on server.

--- cleandata/db_import.py
+++ cleandata/db_import.py
@@ -1,3 +1,4 @@
+from glob import glob
 from os import getcwd, listdir
 from datamapper.app import db
 from datamapper.app.models import Data
@@ -6,7 +7,20 @@ File format: _, date, value, state
 """
 
 def get_file_list(directory = getcwd()):
-    return listdir(directory)
+    return glob('*.csv')
+
+def get_states():
+    with open('states.txt')as st:
+        keys = ('name', 'short_name', 'latitude', 'longitude')
+        states = [dict(zip(keys, state.strip().split(','))) for state in st]
+    return states
+
+def get_data_sets():
+    data_sets = []
+    for ds in get_file_list():
+        data_set = ''.join(file.split('.')[0].replace('_',' '))
+        data_sets.append(data_set)
+    return data_sets
 
 def parse(file, testing=True):
     data_set = ''.join(file.split('.')[0].replace('_',' '))
@@ -19,12 +33,38 @@ def parse(file, testing=True):
         return data[1::int(len(data)*.01)]
     return data
 
-def add_to_db(data):
-    for dp in data:
+def add_states_to_db(states):
+    for state in states:
         try:
-            new_dp = Data(**dp)
-            db.session.add(new_dp)
+            db.session.add(State(**state))
             db.session.commit()
         except Exception as err:
             print(err.message)
             db.session.rollback()
+
+def add_data_sets_to_db(data_sets):
+    for ds in data_sets:
+        try:
+            db.session.add(State(ds))
+            db.session.commit()
+        except Exception as err:
+            print(err.message)
+            db.session.rollback()
+
+def add_data_to_db(data):
+    for dp in data:
+        try:
+            db.session.add(Data(**dp))
+            db.session.commit()
+        except Exception as err:
+            print(err.message)
+            db.session.rollback()
+
+if __name__ == '__main__':
+    files = get_file_list()
+    states = get_states()
+    add_states_to_db(states)
+    data_sets = get_data_sets()
+    add_data_sets_to_db(data_sets)
+    for file in files:
+        add_data_to_db(parse(file))
