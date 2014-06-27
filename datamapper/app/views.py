2b77af0b829461ad29bdb8846d995806f82c9894
Ian Auld <imauld@gmail.com>
2014-06-26 22:26:18 -0700

    Styling updates and some bug fixes

--- datamapper/app/views.py
+++ datamapper/app/views.py
@@ -36,39 +36,52 @@ def format_date(str_date):
     return date(fmt_date.tm_year, fmt_date.tm_mon, fmt_date.tm_mday)
 
 
-def format_data(data):
-    result = [{
-            'raw_data' : d.raw_data,
-            'date' : d.date,
-            'state' : d.state_id}
-            for d in data]
-    states = {s.id:{'lat':s.latitude, 'lng': s.longitude} for s in State.query.all()}
-    for res in result:
-        res['coords'] = states[res['state']]
-    return result
-
-
 @app.route('/get-start-date', methods=['POST', 'GET'])
 def get_start_date():
     data_set = request.args.get('data_set', False)
-    print(data_set)
     first_date = min([dp.date for dp in Data.query.filter(Data.data_set == data_set)])
     first_date = datetime.strftime(first_date, '%m/%d/%Y')
     return jsonify(first_date=first_date)
 
-
-@app.route('/get-data-set', methods=['POST', 'GET'])
-def get_data_set():
+@app.route('/get-date-range', methods=['POST', 'GET'])
+def get_date_range():
     data_set = request.args.get('data_set', False)
     date_start = format_date(request.args.get('date_start', False))
     date_end = format_date(request.args.get('date_end', False))
-    print(date_start, date_end)
     if date_end:
-        result = Data.query.filter(Data.data_set== data_set).filter(Data.date.between(date_start, date_end)).all()
+        result = Data.query.filter(Data.data_set == data_set).filter(Data.date.between(date_start, date_end)).all()
     else:
         result = Data.query.filter(Data.data_set == data_set).filter().all()
-    result = format_data(result)
-    print(result)
+    date_range = sorted(set([str(d.date) for d in result]))
     return jsonify(
-        data = 5
+            date_range=date_range
         )
+
+
+def format_data(data):
+    result = [{
+            'count' : d.raw_data,
+            'state' : d.state_id}
+            for d in data]
+    states = {s.id:{'lat':s.latitude, 'lng': s.longitude} for s in State.query.all()}
+    for res in result:
+        res['lat'] = states[res['state']]['lat']
+        res['lng'] = states[res['state']]['lng']
+        del res['state']
+    print(result)
+    return result
+
+
+@app.route('/get-data-set', methods=['POST', 'GET'])
+def get_data_set():
+    print(request.args)
+    data_set = request.args.get('data_set', False)
+    data_date = request.args.get('date', False)
+    print(data_date)
+    data_date = [int(d) for d in data_date.split('-')]
+    print(data_date)
+    data_date = date(*data_date)
+    data = Data.query.filter(Data.data_set == data_set).filter(Data.date == data_date).all()
+    result = format_data(data)
+    max_val = max([res['count'] for res in result])
+    return jsonify(data=result, max_val=100)
